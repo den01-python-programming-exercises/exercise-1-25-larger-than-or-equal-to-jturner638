ba91fdcbc45c4e3f190db5fa02baff355b00d743
Justin Turner <mrfathed@protonmail.com>
2021-12-04 11:27:17 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+
+    if first > second:
+        print("Greater number is: {}".format(first))
+    elif second > first:
+        print("Greater number is: {}".format(second))
+    else:
+        print("The numbers are equal!")
 
 if __name__ == '__main__':
     main()
